6326c4dd45f1d204dbaf9fb5d103f5881cb9e0c7
Hirotake Yamazoe <72569368+yamazoe@users.noreply.github.com>
2024-05-02 16:06:30 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({})
+
+export default defineConfig({
+  site: 'https://yamazoe.github.io',
+  base: '/yamazoe.github.io',
+})
